5ca0c9d488adf1e05641e7e2bf1274485ba4dcb8
Cruz <A01738823@tec.mx>
2026-04-17 19:55:38 -0600

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Cruz Yael Pérez González on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
